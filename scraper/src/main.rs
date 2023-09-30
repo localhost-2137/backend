83aad3a750d1964f5273f048e4437a5be05dac53
filipton <filipton12@gmail.com>
2023-09-30 14:17:27 +0200

    feat: skeleton of THE scraper

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -8,7 +8,15 @@ async fn main() -> Result<()> {
     let data = tokio::fs::read_to_string("./data.json").await?;
     let universities: Vec<Univertsity> = serde_json::from_str(&data)?;
 
-    println!("{:#?}", universities);
+    let mut tmp_output = String::new();
+    for university in universities {
+        tmp_output += &generate_insert_query(university).await?;
+    }
 
+    tokio::fs::write("./output.sql", tmp_output).await?;
     Ok(())
+}
+
+async fn generate_insert_query(university: Univertsity) -> Result<String> {
+    Ok(format!("INSERT INTO universities({}); \n", university.name))
 }
