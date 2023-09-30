254589377f2620aa0c828bca226c31f931a48f64
filipton <filipton12@gmail.com>
2023-10-01 01:29:58 +0200

    feat: cors

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,16 +2,23 @@ use self::universities::{get_all_cities, get_all_subjects, search};
 use axum::routing::get;
 use axum::Router;
 use sqlx::PgPool;
+use tower_http::cors::{Any, CorsLayer};
 use universities::get_all_universities;
 
 mod distance;
 mod universities;
 
 pub fn router(pool: PgPool) -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_headers(Any)
+        .allow_methods(Any);
+
     Router::new()
         .route("/", get(get_all_universities))
         .route("/cities", get(get_all_cities))
         .route("/subjects", get(get_all_subjects))
         .route("/search", get(search))
+        .layer(cors)
         .with_state(pool)
 }
