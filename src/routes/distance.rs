f791ae041ee4ed611aafc62e3ac7aa961a40a82c
Bartłomiej Strama <kontakt@bstrama.com>
2023-10-01 03:11:28 +0200

    distance endpiont added

--- src/routes/distance.rs
+++ src/routes/distance.rs
@@ -1,38 +1,64 @@
+use axum::Json;
 use reqwest;
-use serde_json::json;
-use serde_json::Value;
+use serde_json::{json, Value};
+
+use axum::extract::Query;
+use serde::Deserialize;
+
 use crate::g_maps::MAPS_API_KEY;
 
-
-// (lng, lat)
-pub async fn get_distance(from: (f32, f32), to: (f32, f32), travel_mode: String) {
-    let body = json!({
-        "origins": vec![generate_waypoints(from)],
-        "destinations": vec![generate_waypoints(to)],
-        "travelMode": travel_mode,
-    });
-
-    let client = reqwest::Client::new();
-    
-    client.post("https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix")
-        .header("Content-Type", "application/json")
-        .header("X-Goog-FieldMask", "originIndex,destinationIndex,duration,distanceMeters,status,condition")
-        .header("X-Goog-Api-Key", MAPS_API_KEY.as_str())
-        .body(serde_json::to_string(&body).unwrap())
-        .send()
-        .await
-        .unwrap();
+#[derive(Deserialize)]
+pub struct LocationQuery {
+    fromLng: f32,
+    fromLat: f32,
+    toLng: f32,
+    toLat: f32,
 }
 
-fn generate_waypoints(lng_lat: (f32, f32)) -> Value {
-    json!({
-      "waypoint": {
-        "location": {
-          "latLng": {
-            "latitude": lng_lat.1,
-            "longitude": lng_lat.0
-          }
-        }
-      }
-    })
+#[derive(Deserialize)]
+struct RootResponse {
+    routes: Vec<RoutesResponse>,
+}
+
+#[derive(Deserialize)]
+struct RoutesResponse {
+    legs: Vec<LegResponse>,
+}
+
+#[derive(Deserialize)]
+struct LegResponse {
+    distance: MeasureResponse,
+    duration: MeasureResponse,
+}
+
+#[derive(Deserialize)]
+struct MeasureResponse {
+    value: u32,
+}
+
+pub async fn distance(location_query: Query<LocationQuery>) -> Json<Value> {
+    let from = (location_query.fromLat, location_query.fromLng);
+    let to = (location_query.toLat, location_query.toLng);
+
+    let result = reqwest::get(format!(
+        "https://maps.googleapis.com/maps/api/directions/json?key={}&origin={},{}&destination={},{}&units=metric",
+        MAPS_API_KEY.as_str(),
+        from.0, from.1,
+        to.0, to.1,
+    ))
+        .await
+        .unwrap();
+
+    let result = result.text().await.unwrap().to_string();
+    println!("{}", result);
+    let result: RootResponse = serde_json::from_str(&result).unwrap();
+
+    let leg = &result.routes[0].legs[0];
+
+    Json(
+        json!({
+            "distance": leg.distance.value,
+            "duration": leg.duration.value,
+        })
+    )
 }
